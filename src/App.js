e98ed74787122b19e8ba818d50f19b8c1982ab6e
MisoDano <dano.miso1@gmail.com>
2024-10-08 18:53:47 +0200

    Deleted unused files and updated existing ones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import GenerateNumbersButton from "./generateNumbers"
 import Tables from "./Tables"
 import { useState } from 'react';
--- src/GenerateCards.js
+++ src/GenerateCards.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
-import "./App.css";
+
+
 import GenerateNumbersButton from "./generateNumbers";
 //import Tables from "./Tables"
 import { useState } from "react";
