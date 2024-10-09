81d0e642287a969e6cd63b22579075d2eb1a4b06
MisoDano <dano.miso1@gmail.com>
2024-10-09 09:20:56 +0200

    router setting changed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-
-import GenerateNumbersButton from "./generateNumbers"
-import Tables from "./Tables"
-import { useState } from 'react';
-import { useEffect } from 'react';
-import ReactDOMServer from 'react-dom/server';
+import GenerateNumbersButton from "./generateNumbers";
+import Tables from "./Tables";
+import { useState } from "react";
+import { useEffect } from "react";
+import ReactDOMServer from "react-dom/server";
 import NumberOfTables from "./NumberOfTables";
 import DeleteNumbersButton from "./DeleteNumbersButton";
 import GenerateCards from "./GenerateCards";
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
-import PlayBingo from "./PlayBingo"
-
-
-
-
+import PlayBingo from "./PlayBingo";
 
 function App() {
-
- 
+  const basename = process.env.NODE_ENV === "production" ? "/bingo" : "";
   return (
-  <>
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<GenerateCards />} />
-          <Route path="/hraj-bingo" element={<PlayBingo />} />
-         </Routes>
-         
-      </div>
-    </Router>
-   
-  </>)
+    <>
+      <Router basename={basename}>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<GenerateCards />} />
+            <Route path="/hraj-bingo" element={<PlayBingo />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
